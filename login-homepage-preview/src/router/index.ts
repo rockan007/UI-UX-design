21b78c201bf5b21406cf5e691c81fc2c0541951a
安琪 <anqi@inspur.com>
2026-06-08 10:28:24 +0800

    feat: restructure admin routes as nested children under AdminLayout

--- login-homepage-preview/src/router/index.ts
+++ login-homepage-preview/src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import AdminLayout from '../layouts/AdminLayout.vue'
 import DashboardView from '../views/DashboardView.vue'
+import UserListView from '../views/UserListView.vue'
+import RoleManageView from '../views/RoleManageView.vue'
+import PermissionView from '../views/PermissionView.vue'
+import OrderManageView from '../views/OrderManageView.vue'
+import SettingsView from '../views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -18,8 +24,40 @@ const router = createRouter({
     },
     {
       path: '/admin',
-      name: 'dashboard',
-      component: DashboardView,
+      component: AdminLayout,
+      redirect: '/admin',
+      children: [
+        {
+          path: '',
+          name: 'dashboard',
+          component: DashboardView,
+        },
+        {
+          path: 'users/list',
+          name: 'user-list',
+          component: UserListView,
+        },
+        {
+          path: 'users/roles',
+          name: 'role-manage',
+          component: RoleManageView,
+        },
+        {
+          path: 'users/permissions',
+          name: 'permissions',
+          component: PermissionView,
+        },
+        {
+          path: 'orders',
+          name: 'order-manage',
+          component: OrderManageView,
+        },
+        {
+          path: 'settings',
+          name: 'settings',
+          component: SettingsView,
+        },
+      ],
     },
   ],
 })
