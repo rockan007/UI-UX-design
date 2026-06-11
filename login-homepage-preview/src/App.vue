44977508a5f717cd87b15a0992c2ad7227be0cde
安琪 <anqi@inspur.com>
2026-06-11 09:59:37 +0800

    feat: wrap App with el-config-provider for reactive locale

--- login-homepage-preview/src/App.vue
+++ login-homepage-preview/src/App.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+import { watch, reactive } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { elLocaleMap } from '@/composables/useLocale'
+
+const { locale } = useI18n({ useScope: 'global' })
+
+const elConfig = reactive({ locale: elLocaleMap[locale.value] || elLocaleMap['zh'] })
+
+watch(locale, (lang) => {
+  elConfig.locale = elLocaleMap[lang] || elLocaleMap['zh']
+})
+</script>
+
 <template>
-  <router-view />
+  <el-config-provider :locale="elConfig.locale">
+    <router-view />
+  </el-config-provider>
 </template>
